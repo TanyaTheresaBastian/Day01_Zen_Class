fc0ae43f92e87e162ad83daee27227325c8b1be7
Tanya Theresa Bastian AK <caeruleaa@gmail.com>
2020-07-29 02:56:59 +0530

    Delete maxArray.js

--- maxArray.js
+++
@@ -1,4 +0,0 @@
-let myArray = [789,456,123,147,258,369,741,852,963,951,753]
-let maxNum = Math.max(...myArray)
-console.log("Array : " + "[789,456,123,147,258,369,741,852,963,951,753]")
-console.log("Max Num: " + maxNum);
