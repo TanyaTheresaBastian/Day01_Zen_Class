27b27017fb79196f77613af9bdfc7e0fcb0ec627
Tanya Theresa Bastian AK <caeruleaa@gmail.com>
2020-07-29 02:56:33 +0530

    Delete sumArray.js

--- sumArray.js
+++
@@ -1,8 +0,0 @@
-let myArray = [789,456,123,147,258,369,741,852,963,951,753]
-let sum =0;
-for (var i =0; i<myArray.length;i++)
-{
-    sum += +myArray[i]
-}
-console.log("Array : " + "[789,456,123,147,258,369,741,852,963,951,753]")
-console.log("Sum : " + sum);
