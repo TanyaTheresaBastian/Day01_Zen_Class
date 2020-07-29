36d9dec955521cf0cee1e44fa8f539ed9234108c
Tanya Theresa Bastian AK <caeruleaa@gmail.com>
2020-07-29 06:30:48 +0530

    Add files via upload

---
+++ 24_07_2020_Day04/typeof.js
@@ -0,0 +1,10 @@
+console.log(typeof 111);
+console.log(typeof 1.11);
+console.log(typeof "111");
+console.log(typeof true);
+console.log(typeof null);
+var a;
+console.log(typeof a);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof function f(){});
